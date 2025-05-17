8d6ba074182ea9085161eb1ec36d65fa8c95ef32
Lucas Silva <125084280+zSukita@users.noreply.github.com>
2025-05-16 21:40:36 -0300

    Initial commit

---
+++ src/lib/reactQuery.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
+
+export const ReactQueryProvider = ({ children }: { children: React.ReactNode }) => (
+  <QueryClientProvider client={queryClient}>
+    {children}
+    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+  </QueryClientProvider>
+);
